b5affe187910c72a25d2998c89c1c1e866a47f76
ZhupanovOlexii <zhupanovolexii@gmail.com>
2020-06-03 21:29:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.15153b8f6d6ca4fa16790bf3db195840.js"
+  "./precache-manifest.0fb1fd1d844c6d5cb05a394e78079e8d.js"
 );
 
 self.addEventListener('message', (event) => {
